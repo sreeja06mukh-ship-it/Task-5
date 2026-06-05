e410daeb76e448961c44d239de1fa76f5d38ba83
Sreeja Mukherjee <sreeja06.mukh@gmail.com>
2026-06-05 17:36:44 +0530

    commit

--- frontend/frontend/src/components/ExpensePieChart.jsx
+++ frontend/frontend/src/components/ExpensePieChart.jsx
@@ -1,9 +1,21 @@
 import {
   PieChart,
   Pie,
+  Cell,
   Tooltip,
+  Legend,
   ResponsiveContainer
 } from "recharts";
+
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#AA66CC",
+  "#FF6666",
+  "#999999"
+];
 
 export default function ExpensePieChart({ data }) {
   return (
@@ -15,8 +27,17 @@ export default function ExpensePieChart({ data }) {
           nameKey="category"
           outerRadius={120}
           label
-        />
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={index}
+              fill={COLORS[index % COLORS.length]}
+            />
+          ))}
+        </Pie>
+
         <Tooltip />
+        <Legend />
       </PieChart>
     </ResponsiveContainer>
   );
